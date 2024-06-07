b166f5fdc90bf7a2ac3895f2b897929951d812ab
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2024-06-07 05:07:21 -0400

    Eliminé Swagger por errores, cree los controles del Rol

---
+++ Logs/src/main/java/cu/edu/cujae/core/services/RolService.java
@@ -0,0 +1,10 @@
+package cu.edu.cujae.core.services;
+
+import cu.edu.cujae.core.clases.Rol;
+import cu.edu.cujae.core.servicesImpl.RolServiceInterface;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RolService implements RolServiceInterface {
+    //Falta implmentar
+}
---
+++ Logs/src/main/java/cu/edu/cujae/core/servicesImpl/RolServiceInterface.java
@@ -0,0 +1,7 @@
+package cu.edu.cujae.core.servicesImpl;
+
+import cu.edu.cujae.core.clases.Rol;
+
+public interface RolServiceInterface {
+    //Falta Implementar
+}
