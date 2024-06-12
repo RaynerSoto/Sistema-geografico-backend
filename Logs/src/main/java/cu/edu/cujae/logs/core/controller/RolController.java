5fe21ff636ef84862c2dd44b8b00531f5dc0e486
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2024-06-11 23:17:21 -0400

    Mapeo de usuarios y roles sin relaciones de manera correcta. Swagger implementado en Logs

--- Logs/src/main/java/cu/edu/cujae/logs/core/controller/RolController.java
+++ Logs/src/main/java/cu/edu/cujae/logs/core/controller/RolController.java
@@ -3,6 +3,8 @@ package cu.edu.cujae.logs.core.controller;
 
 import cu.edu.cujae.logs.core.servicesImpl.RolServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,11 @@ public class RolController {
 
     @Autowired
     private RolServiceInterface rolServiceInterface;
+
+    @GetMapping("/")
+    public ResponseEntity<String> pruena() {
+        return ResponseEntity.ok("Prueba");
+    }
 
 
 }
