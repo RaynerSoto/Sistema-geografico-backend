1414aa46694d9a680c19dc955b7505a261b1a184
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2024-06-27 09:59:07 -0400

    Añadiendo validaciones a PrivilegioRolDto

--- Logs/src/main/java/cu/edu/cujae/logs/core/dto/PrivilegioRolDto.java
+++ Logs/src/main/java/cu/edu/cujae/logs/core/dto/PrivilegioRolDto.java
@@ -1,6 +1,9 @@
 package cu.edu.cujae.logs.core.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,8 +16,14 @@ public class PrivilegioRolDto {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
+    @NotNull(message = "El rol no puede ser null")
+    @NotBlank(message = "El rol no puede estar vacío")
+    @Size(min = 4,max = 50,message = "El rol debe tener entre 4 y 50 caracteres")
     private String rol;
 
+    @NotNull(message = "El código no puede ser null")
+    @Size(min = 1,message = "El código debe tener mínimo 1 caracter")
+    @NotBlank(message = "El código no puede estar vacío")
     private String codigo;
 
 }
