681d73c8db7e27bb9d3ac87909823e3c883a9edf
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2024-07-06 11:46:00 -0400

    Añadiendo seguridad a la parte del login

---
+++ Logs/src/main/java/cu/edu/cujae/logs/core/config/WebSecurityConfiguration.java
@@ -0,0 +1,26 @@
+package cu.edu.cujae.logs.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorize -> authorize
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
