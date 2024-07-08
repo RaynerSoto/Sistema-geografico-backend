7c2135590d46cd65be3bd8ee025797526c705f9c
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2024-07-08 04:33:03 -0400

    Aplicando seguridad, hasta ahora todo aún se encuentra abierto a la entra de externos sin necesidad de loguearse

---
+++ Logs/src/main/java/cu/edu/cujae/logs/core/config/SecurityConfigurations.java
@@ -0,0 +1,20 @@
+package cu.edu.cujae.logs.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
---
+++ Logs/src/main/java/cu/edu/cujae/logs/core/security/AutenticacionService.java
@@ -0,0 +1,19 @@
+package cu.edu.cujae.logs.core.security;
+
+import cu.edu.cujae.logs.core.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacionService implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByUsername(username);
+    }
+}
