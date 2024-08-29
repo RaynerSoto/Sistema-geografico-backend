4b95c8ec152561f87a3e5f116e9d1af511918cb4
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2024-08-29 14:22:32 -0400

    Añadiendo mi FileServices del proyecto anterior

---
+++ Gestion/src/main/java/cu/edu/cujae/gestion/core/excel/FileServices.java
@@ -0,0 +1,38 @@
+package cu.edu.cujae.gestion.core.excel;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class FileServices {
+	//Crear libro sin importar la situaci�n
+	public Workbook construccion_libro(Object object) throws EncryptedDocumentException, IOException {
+		return object instanceof String ? creacion_libro((String)object):creacion_libro((File)object);
+	}
+	
+	
+	//Crear el libro de Excel a partir de la direcci�n
+	public Workbook creacion_libro(String direccion) throws EncryptedDocumentException, IOException {
+		return WorkbookFactory.create(new File(direccion));
+	}
+	
+	//Crear libro a partir del fichero
+	public Workbook creacion_libro(File file) throws EncryptedDocumentException, IOException {
+		return WorkbookFactory.create(file);
+	}
+	
+	
+	//Listado hojas de un libro
+	public ArrayList<Sheet> listado_hojas(Workbook libro){
+		ArrayList<Sheet>hojas = new ArrayList<Sheet>();
+		for(int contador = 0; contador<libro.getNumberOfSheets();contador++) {
+			hojas.add((Sheet) libro.getSheetAt(contador));
+		}
+		return hojas;
+	}
+}
